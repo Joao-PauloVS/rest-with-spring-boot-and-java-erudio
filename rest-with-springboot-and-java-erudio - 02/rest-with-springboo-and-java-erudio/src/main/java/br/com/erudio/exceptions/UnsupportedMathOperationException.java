5bd78770660fe4272096d38c0b5c118e7b58fe4a
João Paulo <joao.paulojgt@gmail.com>
2024-01-03 16:35:08 -0300

    inicio da calculadora utilizando request mapping

---
+++ rest-with-springboot-and-java-erudio - 02/rest-with-springboo-and-java-erudio/src/main/java/br/com/erudio/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,9 @@
+package br.com.erudio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+    private final static long serialVersionUID = 1L;
+}
---
+++ rest-with-springboot-and-java-erudio - 02/rest-with-springboo-and-java-erudio/src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,10 @@
+package br.com.erudio.exceptions.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+}
